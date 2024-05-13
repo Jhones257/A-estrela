7100b3b208ff1ca01023f850d36fedfbb469b947
Jhones de Sousa <mynamejhones@gmail.com>
2024-05-13 09:13:00 -0300

    inserção do código modified

--- astar.py
+++ astar.py
@@ -2,102 +2,102 @@ import pygame
 import math
 from queue import PriorityQueue
 
-WIDTH = 800
-WIN = pygame.display.set_mode((WIDTH, WIDTH))
-pygame.display.set_caption("A* Path Finding Algorithm")
+LARGURA = 800
+JANELA = pygame.display.set_mode((LARGURA, LARGURA))
+pygame.display.set_caption("Path Finding - Algoritmo de busca A*")
 
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 255, 0)
-YELLOW = (255, 255, 0)
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-PURPLE = (128, 0, 128)
-ORANGE = (255, 165 ,0)
-GREY = (128, 128, 128)
-TURQUOISE = (64, 224, 208)
+VERMELHO = (255, 0, 0)
+VERDE = (0, 255, 0)
+AZUL = (0, 255, 0)
+AMARELO = (255, 255, 0)
+BRANCO = (255, 255, 255)
+PRETO = (0, 0, 0)
+ROXO = (128, 0, 128)
+LARANJA = (255, 165 ,0)
+CINZA = (128, 128, 128)
+TURQUESA = (64, 224, 208)
 
-class Spot:
-	def __init__(self, row, col, width, total_rows):
-		self.row = row
-		self.col = col
-		self.x = row * width
-		self.y = col * width
-		self.color = WHITE
-		self.neighbors = []
-		self.width = width
-		self.total_rows = total_rows
+class Grafos:
+	def __init__(self, linha, coluna, LARGURA, total_linhas):
+		self.linha = linha
+		self.coluna = coluna
+		self.x = linha * LARGURA
+		self.y = coluna * LARGURA
+		self.color = BRANCO
+		self.vizinhos = []
+		self.LARGURA = LARGURA
+		self.total_linhas = total_linhas
 
 	def get_pos(self):
-		return self.row, self.col
+		return self.linha, self.coluna
 
 	def is_closed(self):
-		return self.color == RED
+		return self.color ==VERMELHO
 
 	def is_open(self):
-		return self.color == GREEN
+		return self.color == VERDE
 
 	def is_barrier(self):
-		return self.color == BLACK
+		return self.color == PRETO
 
 	def is_start(self):
-		return self.color == ORANGE
+		return self.color == LARANJA
 
 	def is_end(self):
-		return self.color == TURQUOISE
+		return self.color == TURQUESA
 
 	def reset(self):
-		self.color = WHITE
+		self.color = BRANCO
 
 	def make_start(self):
-		self.color = ORANGE
+		self.color = LARANJA
 
 	def make_closed(self):
-		self.color = RED
+		self.color =VERMELHO
 
 	def make_open(self):
-		self.color = GREEN
+		self.color = VERDE
 
 	def make_barrier(self):
-		self.color = BLACK
+		self.color = PRETO
 
 	def make_end(self):
-		self.color = TURQUOISE
+		self.color = TURQUESA
 
 	def make_path(self):
-		self.color = PURPLE
+		self.color = ROXO
 
-	def draw(self, win):
-		pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.width))
+	def draw(self, janela):
+		pygame.draw.rect(janela, self.color, (self.x, self.y, self.LARGURA, self.LARGURA))
 
-	def update_neighbors(self, grid):
-		self.neighbors = []
-		if self.row < self.total_rows - 1 and not grid[self.row + 1][self.col].is_barrier(): # DOWN
-			self.neighbors.append(grid[self.row + 1][self.col])
+	def atualizar_vizinhos(self, grid):
+		self.vizinhos = []
+		if self.linha < self.total_linhas - 1 and not grid[self.linha + 1][self.coluna].is_barrier():
+			self.vizinhos.append(grid[self.linha + 1][self.coluna])
 
-		if self.row > 0 and not grid[self.row - 1][self.col].is_barrier(): # UP
-			self.neighbors.append(grid[self.row - 1][self.col])
+		if self.linha > 0 and not grid[self.linha - 1][self.coluna].is_barrier(): 
+			self.vizinhos.append(grid[self.linha - 1][self.coluna])
 
-		if self.col < self.total_rows - 1 and not grid[self.row][self.col + 1].is_barrier(): # RIGHT
-			self.neighbors.append(grid[self.row][self.col + 1])
+		if self.coluna < self.total_linhas - 1 and not grid[self.linha][self.coluna + 1].is_barrier(): # RIGHT
+			self.vizinhos.append(grid[self.linha][self.coluna + 1])
 
-		if self.col > 0 and not grid[self.row][self.col - 1].is_barrier(): # LEFT
-			self.neighbors.append(grid[self.row][self.col - 1])
+		if self.coluna > 0 and not grid[self.linha][self.coluna - 1].is_barrier(): # LEFT
+			self.vizinhos.append(grid[self.linha][self.coluna - 1])
 
 	def __lt__(self, other):
 		return False
 
-
+#Função utilizada para realizar o cálculo do valor h(x)(Heurística)
 def h(p1, p2):
 	x1, y1 = p1
 	x2, y2 = p2
 	return abs(x1 - x2) + abs(y1 - y2)
 
 
-def reconstruct_path(came_from, current, draw):
-	while current in came_from:
-		current = came_from[current]
-		current.make_path()
+def caminho_reconstruido(veio_de, atual, draw):
+	while atual in veio_de:
+		atual = veio_de[atual]
+		atual.make_path()
 		draw()
 
 
@@ -106,9 +106,9 @@ def algorithm(draw, grid, start, end):
 	open_set = PriorityQueue()
 	open_set.put((0, count, start))
 	came_from = {}
-	g_score = {spot: float("inf") for row in grid for spot in row}
+	g_score = {spot: float("inf") for linha in grid for spot in linha}
 	g_score[start] = 0
-	f_score = {spot: float("inf") for row in grid for spot in row}
+	f_score = {spot: float("inf") for linha in grid for spot in linha}
 	f_score[start] = h(start.get_pos(), end.get_pos())
 
 	open_set_hash = {start}
@@ -118,94 +118,94 @@ def algorithm(draw, grid, start, end):
 			if event.type == pygame.QUIT:
 				pygame.quit()
 
-		current = open_set.get()[2]
-		open_set_hash.remove(current)
+		atual = open_set.get()[2]
+		open_set_hash.remove(atual)
 
-		if current == end:
-			reconstruct_path(came_from, end, draw)
+		if atual == end:
+			caminho_reconstruido(came_from, end, draw)
 			end.make_end()
 			return True
 
-		for neighbor in current.neighbors:
-			temp_g_score = g_score[current] + 1
+		for vizinho in atual.vizinhos:
+			temp_g_score = g_score[atual] + 1
 
-			if temp_g_score < g_score[neighbor]:
-				came_from[neighbor] = current
-				g_score[neighbor] = temp_g_score
-				f_score[neighbor] = temp_g_score + h(neighbor.get_pos(), end.get_pos())
-				if neighbor not in open_set_hash:
+			if temp_g_score < g_score[vizinho]:
+				came_from[vizinho] = atual
+				g_score[vizinho] = temp_g_score
+				f_score[vizinho] = temp_g_score + h(vizinho.get_pos(), end.get_pos())
+				if vizinho not in open_set_hash:
 					count += 1
-					open_set.put((f_score[neighbor], count, neighbor))
-					open_set_hash.add(neighbor)
-					neighbor.make_open()
+					open_set.put((f_score[vizinho], count, vizinho))
+					open_set_hash.add(vizinho)
+					vizinho.make_open()
 
 		draw()
 
-		if current != start:
-			current.make_closed()
+		if atual != start:
+			atual.make_closed()
 
 	return False
 
 
-def make_grid(rows, width):
+def make_grid(linhas, LARGURA):
 	grid = []
-	gap = width // rows
-	for i in range(rows):
+	gap = LARGURA // linhas
+	for i in range(linhas):
 		grid.append([])
-		for j in range(rows):
-			spot = Spot(i, j, gap, rows)
+		for j in range(linhas):
+			spot = Grafos(i, j, gap, linhas)
 			grid[i].append(spot)
 
 	return grid
 
 
-def draw_grid(win, rows, width):
-	gap = width // rows
-	for i in range(rows):
-		pygame.draw.line(win, GREY, (0, i * gap), (width, i * gap))
-		for j in range(rows):
-			pygame.draw.line(win, GREY, (j * gap, 0), (j * gap, width))
+def draw_grid(janela, linhas, LARGURA):
+	gap = LARGURA // linhas
+	for i in range(linhas):
+		pygame.draw.line(janela, CINZA, (0, i * gap), (LARGURA, i * gap))
+		for j in range(linhas):
+			pygame.draw.line(janela, CINZA, (j * gap, 0), (j * gap, LARGURA))
 
 
-def draw(win, grid, rows, width):
-	win.fill(WHITE)
+def draw(janela, grid, linhas, LARGURA):
+	janela.fill(BRANCO)
 
-	for row in grid:
-		for spot in row:
-			spot.draw(win)
+	for linha in grid:
+		for spot in linha:
+			spot.draw(janela)
 
-	draw_grid(win, rows, width)
+	draw_grid(janela, linhas, LARGURA)
 	pygame.display.update()
 
 
-def get_clicked_pos(pos, rows, width):
-	gap = width // rows
+def get_clicked_pos(pos, linhas, LARGURA):
+	gap = LARGURA // linhas
 	y, x = pos
 
-	row = y // gap
-	col = x // gap
+	linha = y // gap
+	coluna = x // gap
 
-	return row, col
+	return linha, coluna
 
 
-def main(win, width):
+def main(janela, LARGURA):
 	ROWS = 50
-	grid = make_grid(ROWS, width)
+	grid = make_grid(ROWS, LARGURA)
 
 	start = None
 	end = None
 
 	run = True
 	while run:
-		draw(win, grid, ROWS, width)
+		draw(janela, grid, ROWS, LARGURA)
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				run = False
 
-			if pygame.mouse.get_pressed()[0]: # LEFT
+			if pygame.mouse.get_pressed()[0]: 
 				pos = pygame.mouse.get_pos()
-				row, col = get_clicked_pos(pos, ROWS, width)
-				spot = grid[row][col]
+				linha, coluna = get_clicked_pos(pos, ROWS, LARGURA)
+				spot = grid[linha][coluna]
 				if not start and spot != end:
 					start = spot
 					start.make_start()
@@ -217,10 +217,10 @@ def main(win, width):
 				elif spot != end and spot != start:
 					spot.make_barrier()
 
-			elif pygame.mouse.get_pressed()[2]: # RIGHT
+			elif pygame.mouse.get_pressed()[2]: 
 				pos = pygame.mouse.get_pos()
-				row, col = get_clicked_pos(pos, ROWS, width)
-				spot = grid[row][col]
+				linha, coluna = get_clicked_pos(pos, ROWS, LARGURA)
+				spot = grid[linha][coluna]
 				spot.reset()
 				if spot == start:
 					start = None
@@ -229,17 +229,17 @@ def main(win, width):
 
 			if event.type == pygame.KEYDOWN:
 				if event.key == pygame.K_SPACE and start and end:
-					for row in grid:
-						for spot in row:
-							spot.update_neighbors(grid)
+					for linha in grid:
+						for spot in linha:
+							spot.atualizar_vizinhos(grid)
 
-					algorithm(lambda: draw(win, grid, ROWS, width), grid, start, end)
+					algorithm(lambda: draw(janela, grid, ROWS, LARGURA), grid, start, end)
 
 				if event.key == pygame.K_c:
 					start = None
 					end = None
-					grid = make_grid(ROWS, width)
+					grid = make_grid(ROWS, LARGURA)
 
 	pygame.quit()
 
-main(WIN, WIDTH)
+main(JANELA, LARGURA)
